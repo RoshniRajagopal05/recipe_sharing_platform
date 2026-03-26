19274d4af181fb4838c20f29c7b859efa7d2da26
roshni@005 <roshnirajagopal6@gmail.com>
2026-03-26 10:30:38 +0530

    Add Procfile for deployment and enhance RecipeSerializer to include image upload URL

--- userapi/serializers.py
+++ userapi/serializers.py
@@ -9,7 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     user=UserSerializer(read_only=True)
+    image_upload = serializers.SerializerMethodField()
     class Meta:
         model = Recipe
         fields = '__all__'
+    def get_image_upload(self, obj):
+        if obj.image_upload:
+            return obj.image_upload.url
+        return None
 
